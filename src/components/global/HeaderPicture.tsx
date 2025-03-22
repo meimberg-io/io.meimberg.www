9834c07c455e2d2e7365500ae3ae7a3b4159449a
OliverMeimberg <oliver.meimberg@gmail.com>
2025-03-22 01:10:20 +0100

    headerpicture

---
+++ src/components/global/HeaderPicture.tsx
@@ -0,0 +1,29 @@
+import { AssetStoryblok } from '@/types/component-types-sb'
+import clsx from 'clsx'
+import Image from 'next/image'
+
+
+export default function HeaderPicture({ headerpicture }: {headerpicture?:AssetStoryblok}) {
+
+	return (
+		<>
+			{
+				headerpicture?.filename && (
+
+				<div  className='sm:px-8'>
+
+					<div className="mx-auto w-full max-w-7xl lg:px-8 mt-16  " >
+						<div  className={clsx('relative px-4 sm:px-8 lg:px-12')} style={{height:"240px"}}>
+
+							<Image src={headerpicture.filename + `/m/1216x240` + `/filters:focal(${headerpicture.focus})`}  alt="Headerpicture"
+										 className="w-full object-center object-fill"   fill={true}
+										 layout='fill'
+										 objectFit='cover'/>
+						</div>
+					</div>
+				</div>
+		)}
+</>
+	)
+
+}
--- src/components/layout/StuffLayout.tsx
+++ src/components/layout/StuffLayout.tsx
@@ -3,13 +3,12 @@
 import { useContext } from 'react'
 import { useRouter } from 'next/navigation'
 
-
 import { Container } from '@/components/layout/Container.tsx'
 import { formatDate } from '@/lib/formatDate.ts'
 import { AppContext } from '@/lib/providers.tsx'
 import { ArticleStoryblok } from '@/types/component-types-sb'
 import { ArrowLeftIcon } from '@/components/Svg.tsx'
-
+import HeaderPicture from '@/components/global/HeaderPicture.tsx'
 
 
 export function StuffLayout({ article, children }: {
@@ -19,19 +18,21 @@ export function StuffLayout({ article, children }: {
 	const router = useRouter()
 	const { previousPathname } = useContext(AppContext)
 	return (
-		<Container className="mt-16 lg:mt-32">
-			<div className="xl:relative">
-				<div className="mx-auto max-w-2xl">
-					{previousPathname && (
-						<button
-							type="button"
-							onClick={() => router.back()}
-							aria-label="Go back to articles"
-							className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white ring-1 shadow-md shadow-zinc-800/5 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-						>
-							<ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
-						</button>
-					)}
+		<>
+			<HeaderPicture headerpicture={article.headerpicture} />
+			<Container className="mt-16 lg:mt-32">
+				<div className="xl:relative">
+					<div className="mx-auto max-w-2xl">
+						{previousPathname && (
+							<button
+								type="button"
+								onClick={() => router.back()}
+								aria-label="Go back to articles"
+								className="group mb-8 flex h-10 w-10 items-center justify-center rounded-full bg-white ring-1 shadow-md shadow-zinc-800/5 ring-zinc-900/5 transition lg:absolute lg:-left-5 lg:-mt-2 lg:mb-0 xl:-top-1.5 xl:left-0 xl:mt-0 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
+							>
+								<ArrowLeftIcon className="h-4 w-4 stroke-zinc-500 transition group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400" />
+							</button>
+						)}
 
 						<article>
 							<header className="flex flex-col">
@@ -44,13 +45,13 @@ export function StuffLayout({ article, children }: {
 								</time>
 							</header>
 
-									{children}
+							{children}
 
 						</article>
 					</div>
 
-			</div>
-		</Container>
-
+				</div>
+			</Container>
+		</>
 	)
 }
