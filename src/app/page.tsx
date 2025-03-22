586e5e6561ac93d32b2c07a010b5bbd6a720389f
OliverMeimberg <oliver.meimberg@gmail.com>
2025-03-23 00:24:58 +0100

    linter fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { renderPage } from '@/lib/pageRenderer.tsx'
 
 
-export default async function StoryPage({ params, searchParams }: {
-	params: { slug: string[] }
-	searchParams: { [key: string]: string | string[] | undefined }
-}) {
-	return renderPage(params.slug, Array.isArray(searchParams.secret) ? searchParams.secret[0] : searchParams.secret)
+export default async function StoryPage({ params, searchParams }: any) {
+	const secret = Array.isArray(searchParams?.secret) ? searchParams.secret[0] : searchParams?.secret
+	return renderPage(params?.slug, secret)
 }
