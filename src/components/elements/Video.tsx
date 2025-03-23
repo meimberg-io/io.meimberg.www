b8417461d7a7424d177faf35a4173811ef4389a7
OliverMeimberg <oliver.meimberg@gmail.com>
2025-03-23 14:36:30 +0100

    youtube

---
+++ src/components/elements/Video.tsx
@@ -0,0 +1,10 @@
+import {  VideoStoryblok } from '@/types/component-types-sb'
+
+
+export default function Video({ blok }: VideoStoryblok) {
+
+	return (
+		<div>Component not completed. {blok.id}</div>
+	)
+
+}
