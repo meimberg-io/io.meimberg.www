decd176b9141f51853601f72ee8ec448b79ffbd1
OliverMeimberg <oliver.meimberg@gmail.com>
2025-03-23 17:42:55 +0100

    fix: hyperlink

--- src/components/elements/Hyperlink.tsx
+++ src/components/elements/Hyperlink.tsx
@@ -2,14 +2,17 @@ import ElementWrapper from '@/components/layout/ElementWrapper.tsx'
 import { LinkIcon } from '@/components/util/Svg.tsx'
 import Link from 'next/link'
 import { HyperlinkStoryblok } from '@/types/component-types-sb'
+import { storyblokEditable } from '@storyblok/react/rsc'
 
 
 export default function Hyperlink({ blok }: HyperlinkStoryblok) {
 
 	return (
 		<ElementWrapper>
-			<div className="rounded-2xl  px-4 py-4 border bg-zinc-100 dark:bg-zinc-950 border-gray-200 dark:border-gray-800 ">
-				<Link href={blok.url} target="_blank"
+
+
+			<div className="rounded-2xl  px-4 py-4 border bg-zinc-100 dark:bg-zinc-950 border-gray-200 dark:border-gray-800 " {...storyblokEditable(blok)} >
+				<Link href={blok.url.url} target="_blank"
 							className="relative transition flex text-sm font-medium hover:text-zinc-400  text-zink-200 dark:text-teal-500">
 					<LinkIcon className="h-6 w-6 flex-none" />
 					<span className="ml-2">{blok.label}</span>
