815f471090e8d3a102385592a5128f328fdf4a8d
OliverMeimberg <oliver.meimberg@gmail.com>
2025-03-24 09:28:46 +0100

    dark mode per default

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -4,52 +4,53 @@ import { createContext, useEffect, useRef } from 'react'
 import { usePathname } from 'next/navigation'
 import { ThemeProvider, useTheme } from 'next-themes'
 
+
 function usePrevious<T>(value: T) {
 	const ref = useRef<T>(undefined)
 
-  useEffect(() => {
-    ref.current = value
-  }, [value])
+	useEffect(() => {
+		ref.current = value
+	}, [value])
 
-  return ref.current
+	return ref.current
 }
 
 function ThemeWatcher() {
 	const { resolvedTheme, setTheme } = useTheme()
 
-  useEffect(() => {
+	useEffect(() => {
 		const media = window.matchMedia('(prefers-color-scheme: dark)')
 
-    function onMediaChange() {
+		function onMediaChange() {
 			const systemTheme = media.matches ? 'dark' : 'light'
-      if (resolvedTheme === systemTheme) {
-        setTheme('system')
-      }
-    }
+			if (resolvedTheme === systemTheme) {
+				setTheme('system')
+			}
+		}
 
-    onMediaChange()
-    media.addEventListener('change', onMediaChange)
+		onMediaChange()
+		media.addEventListener('change', onMediaChange)
 
-    return () => {
-      media.removeEventListener('change', onMediaChange)
-    }
-  }, [resolvedTheme, setTheme])
+		return () => {
+			media.removeEventListener('change', onMediaChange)
+		}
+	}, [resolvedTheme, setTheme])
 
-  return null
+	return null
 }
 
 export const AppContext = createContext<{ previousPathname?: string | null }>({})
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
+	const pathname = usePathname()
 	const previousPathname = usePrevious(pathname)
 
 	return (
-    <AppContext.Provider value={{ previousPathname  }}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        <ThemeWatcher />
-        {children}
-      </ThemeProvider>
-    </AppContext.Provider>
-  )
+		<AppContext.Provider value={{ previousPathname }}>
+			<ThemeProvider attribute="class" disableTransitionOnChange defaultTheme="dark" enableSystem={false}>
+				<ThemeWatcher />
+				{children}
+			</ThemeProvider>
+		</AppContext.Provider>
+	)
 }
