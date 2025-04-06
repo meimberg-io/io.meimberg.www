111da02b662a5940cd954b0a1d29969d1ee8a345
OliverMeimberg <oli@meimberg.io>
2025-04-06 23:42:50 +0200

    tracking deaktivieren

--- src/components/util/MatomoTracker.tsx
+++ src/components/util/MatomoTracker.tsx
@@ -1,28 +1,35 @@
-"use client"
+'use client'
 
-import { useEffect } from "react"
+import { useEffect } from 'react'
+
+/*
+    Tracking deaktivieren und aktivieren:
+    javascript:localStorage.setItem("matomo-disable","true");alert("Tracking deaktiviert!");
+    javascript:localStorage.removeItem("matomo-disable");alert("Tracking wieder aktiv!");
+*/
 
 export function MatomoTracker() {
-    const enabled = process.env.NEXT_PUBLIC_MATOMO_TRACKER === "true"
+	const enabled = process.env.NEXT_PUBLIC_MATOMO_TRACKER === 'true'
+
+	useEffect(() => {
+		if (!enabled) return
+		if (localStorage.getItem("matomo-disable") === "true") return
 
 
-    useEffect(() => {
-        if (!enabled) return
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-expect-error
+		const _paq = (window._paq = window._paq || [])
+		_paq.push(['trackPageView'])
+		_paq.push(['disableCookies'])
+		_paq.push(['enableLinkTracking'])
+		_paq.push(['setTrackerUrl', 'https://matomo.meimberg.io/matomo.php'])
+		_paq.push(['setSiteId', '2']) // deine Site-ID
 
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-expect-error
-        const _paq = (window._paq = window._paq || [])
-        _paq.push(["trackPageView"])
-        _paq.push(["disableCookies"])
-        _paq.push(["enableLinkTracking"])
-        _paq.push(["setTrackerUrl", "https://matomo.meimberg.io/matomo.php"])
-        _paq.push(["setSiteId", "2"]) // deine Site-ID
+		const d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0]
+		g.async = true
+		g.src = 'https://matomo.meimberg.io/matomo.js'
+		s.parentNode?.insertBefore(g, s)
+	}, [])
 
-        const d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0]
-        g.async = true
-        g.src = "https://matomo.meimberg.io/matomo.js"
-        s.parentNode?.insertBefore(g, s)
-    }, [])
-
-    return null
+	return null
 }
