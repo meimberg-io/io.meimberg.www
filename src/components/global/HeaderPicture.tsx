4c0ecbc7d8f0d0b6f7f15035b485299f1c06c905
OliverMeimberg <oli@meimberg.io>
2025-09-28 19:26:57 +0200

    linter fixes

--- src/components/global/HeaderPicture.tsx
+++ src/components/global/HeaderPicture.tsx
@@ -14,11 +14,14 @@ export default function HeaderPicture({ headerpicture }: {headerpicture?:AssetSt
 
 					<div className="mx-auto w-full max-w-7xl lg:px-8 mt-16  " >
 						<div  className={clsx('relative px-4 sm:px-8 lg:px-12')} style={{height:"240px"}}>
-
-							<Image src={headerpicture.filename + `/m/1216x240` + `/filters:focal(${headerpicture.focus})`}  alt="Headerpicture"
-										 className="w-full object-center object-fill"   fill={true}
-										 layout='fill'
-										 objectFit='cover'/>
+							{headerpicture.filename ? (
+								<Image src={headerpicture.filename + `/m/1216x240` + `/filters:focal(${headerpicture.focus})`}  alt="Headerpicture"
+											 className="w-full object-center object-fill"   fill={true}
+											 layout='fill'
+											 objectFit='cover'/>
+							) : (
+								<div className="w-full h-full bg-gray-200" />
+							)}
 						</div>
 					</div>
 				</div>
