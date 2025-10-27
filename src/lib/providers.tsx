eb089c75143378acf57d6bfc01449b62d1bacb69
OliverMeimberg <oliver.meimberg@gmail.com>
2025-10-27 22:43:15 +0100

    fix darkmode on mobile

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -2,7 +2,7 @@
 
 import { createContext, useEffect, useRef } from 'react'
 import { usePathname } from 'next/navigation'
-import { ThemeProvider, useTheme } from 'next-themes'
+import { ThemeProvider } from 'next-themes'
 
 
 function usePrevious<T>(value: T) {
@@ -15,30 +15,6 @@ function usePrevious<T>(value: T) {
 	return ref.current
 }
 
-function ThemeWatcher() {
-	const { resolvedTheme, setTheme } = useTheme()
-
-	useEffect(() => {
-		const media = window.matchMedia('(prefers-color-scheme: dark)')
-
-		function onMediaChange() {
-			const systemTheme = media.matches ? 'dark' : 'light'
-			if (resolvedTheme === systemTheme) {
-				setTheme('system')
-			}
-		}
-
-		onMediaChange()
-		media.addEventListener('change', onMediaChange)
-
-		return () => {
-			media.removeEventListener('change', onMediaChange)
-		}
-	}, [resolvedTheme, setTheme])
-
-	return null
-}
-
 export const AppContext = createContext<{ previousPathname?: string | null }>({})
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -48,7 +24,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<AppContext.Provider value={{ previousPathname }}>
 			<ThemeProvider attribute="class" disableTransitionOnChange defaultTheme="dark" enableSystem={false}>
-				<ThemeWatcher />
 				{children}
 			</ThemeProvider>
 		</AppContext.Provider>
