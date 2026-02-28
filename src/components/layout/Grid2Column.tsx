ac52f0343ad76d0a0ae602e5c8cd26a8ab934c49
OliverMeimberg <oli@meimberg.io>
2026-02-28 22:56:25 +0100

    Refactor Grid2Column layout and update styles
    - Cleaned up the Grid2Column component structure for better readability.
    - Removed unnecessary line breaks in the CSS for the hr element in om.css.
    - Simplified the tailwind.css file by removing redundant Tailwind directives.

--- src/components/layout/Grid2Column.tsx
+++ src/components/layout/Grid2Column.tsx
@@ -8,20 +8,20 @@ export default function Grid2Column({ blok }: { blok: Grid2ColumnStoryblok }) {
 	return (
 		<ElementWrapper spacing="large">
 		<div className="grid grid-cols-1 gap-y-12 lg:grid-cols-2 lg:items-start">
-			<div className="flex flex-col gap-12">
-				{blok.col1?.map((nestedBlok: any) => (
-					<div key={nestedBlok._uid}>
-						<StoryblokServerComponent blok={nestedBlok} />
-					</div>
-				))}
-			</div>
-			<div className="flex flex-col gap-12 lg:pl-20">
-				{blok.col2?.map((nestedBlok: any) => (
-					<div key={nestedBlok._uid}>
-						<StoryblokServerComponent blok={nestedBlok} />
-					</div>
-				))}
-			</div>
+		<div className="flex flex-col gap-12">
+			{blok.col1?.map((nestedBlok: any) => (
+				<div key={nestedBlok._uid}>
+					<StoryblokServerComponent blok={nestedBlok} />
+				</div>
+			))}
+		</div>
+		<div className="flex flex-col gap-12 lg:pl-20">
+			{blok.col2?.map((nestedBlok: any) => (
+				<div key={nestedBlok._uid}>
+					<StoryblokServerComponent blok={nestedBlok} />
+				</div>
+			))}
+		</div>
 		</div>
 		</ElementWrapper>
 	)
