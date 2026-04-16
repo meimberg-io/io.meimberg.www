49373520258204ed9529f450b22edbd08a45d9d5
oliver.meimberg <oli@meimberg.io>
2026-04-16 22:59:08 +0200

    refactor: update typography in Header component for consistency
    - Changed font class to use 'font-headline' in Desktop and Mobile Navigation sections of the Header component for improved typography consistency across the application.

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -65,7 +65,7 @@ function DesktopNavigation({
 	return (
 		<nav {...props}>
 			<ul
-				className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+				className="flex rounded-full bg-white/90 px-3 text-sm font-headline font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
 
 
 			{getTopNavItems(topnav).map((item) => (
@@ -90,7 +90,7 @@ function MobileNavigation(
 	return (
 		<Popover {...props}>
 			<PopoverButton
-				className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+				className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-headline font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
 				Menu
 				<ChevronDownIcon className="ml-3 h-auto w-2 stroke-zinc-500 group-hover:stroke-zinc-700 dark:group-hover:stroke-zinc-400" />
 			</PopoverButton>
@@ -112,7 +112,7 @@ function MobileNavigation(
 					</h2>
 				</div>
 				<nav className="mt-6">
-					<ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+					<ul className="-my-2 divide-y divide-zinc-100 text-base font-headline text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
 
 
 					{getTopNavItems(topnav).map((item) => (
